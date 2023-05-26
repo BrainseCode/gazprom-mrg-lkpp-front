917fa9329743c111ca9f026737393435c284c3eb
tpiv <ds@tpiv.ru>
2023-05-26 11:49:59 +0300

    страница авторизации (login)

--- src/Pages/Auth/login/Index.tsx
+++ src/Pages/Auth/login/Index.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import './login.css';
+import login from '../../../assets/images/login.png';
+
+import { useLocation, useNavigate } from 'react-router-dom';
 
 export default function Index() {
-    return (
-        <div>
-           <div className='w-96 h-96 bg-blue-300 text-white shadow-lg shadow-white-500/40'>
-            ufpghjv
-           </div>
+  const navigate = useNavigate();
+  const location = useLocation();
+  const fromPage = location.state?.from.pathname || '/';
+
+  return (
+    <div className="flex items-center justify-center h-screen gazprom-baground">
+      <div className="w-96 h-96 bg-white text-black shadow-lg shadow-white-500/40 rounded">
+        <div className="flex flex-col justify-center items-center">
+          <div>
+            <img src={login} alt="" />
+          </div>
+          <div className='mt-4'>
+            <div className='flex flex-col justify-between mt-1'>
+                <label htmlFor="email" className="block text-sm text-left font-medium text-gray-700">
+                Введите логин
+                </label>
+                <div className="mt-1">
+                <input
+                    type="text"
+                    name="agent"
+                    id="agent"
+                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    placeholder="ИНН"
+                />
+                </div>
+            </div>
+            <div className='flex flex-col justify-between mt-4'>
+                <label htmlFor="email" className="block text-sm text-left font-medium text-gray-700">
+                Введите пароль
+                </label>
+                <div className="mt-1">
+                <input
+                type="text"
+                name="face"
+                id="face"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                placeholder="Пароль"
+                />
+                </div>
+            </div>
+          </div>
+          <div className='mt-6 mb-2'>
+            <button className="btn btn-blue text-center gazprom-button w-[170px]">Войти</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
