a51caa3667478654cffd53ac9f0b9bcd0289f68b
tpiv <ds@tpiv.ru>
2023-05-26 12:04:59 +0300

    страница авторизации (login)v2

--- src/Pages/Auth/login/Index.tsx
+++ src/Pages/Auth/login/Index.tsx
@@ -8,6 +8,10 @@ export default function Index() {
   const navigate = useNavigate();
   const location = useLocation();
   const fromPage = location.state?.from.pathname || '/';
+  const auch = (event) => {
+    event.preventDefault();
+    alert(event.currentTarget[0].value+' '+event.currentTarget[1].value);
+  }
 
   return (
     <div className="flex items-center justify-center h-screen gazprom-baground">
@@ -16,39 +20,43 @@ export default function Index() {
           <div>
             <img src={login} alt="" />
           </div>
-          <div className='mt-4'>
-            <div className='flex flex-col justify-between mt-1'>
+          <form onSubmit={auch}>
+            <div className="mt-4">
+              <div className="flex flex-col justify-between mt-1">
                 <label htmlFor="email" className="block text-sm text-left font-medium text-gray-700">
-                Введите логин
+                  Введите логин
                 </label>
                 <div className="mt-1">
-                <input
+                  <input
                     type="text"
                     name="agent"
                     id="agent"
                     className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
                     placeholder="ИНН"
-                />
+                    defaultValue=""
+                  />
                 </div>
-            </div>
-            <div className='flex flex-col justify-between mt-4'>
+              </div>
+              <div className="flex flex-col justify-between mt-4">
                 <label htmlFor="email" className="block text-sm text-left font-medium text-gray-700">
-                Введите пароль
+                  Введите пароль
                 </label>
                 <div className="mt-1">
-                <input
-                type="text"
-                name="face"
-                id="face"
-                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                placeholder="Пароль"
-                />
+                  <input
+                    type="password"
+                    name="face"
+                    id="face"
+                    className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                    placeholder="Пароль"
+                    defaultValue=""
+                  />
                 </div>
+              </div>
             </div>
-          </div>
-          <div className='mt-6 mb-2'>
-            <button className="btn btn-blue text-center gazprom-button w-[170px]">Войти</button>
-          </div>
+            <div className="mt-6 mb-2">
+              <button className="btn btn-blue text-center gazprom-button w-[170px]">Войти</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
